ecf0e8085f886b849c7c60e02d25767e3d9dd92c
AdityRiya <66531291+AdityRiya@users.noreply.github.com>
2022-09-24 21:49:52 +0600

    update 2

--- r_p_s.py
+++ r_p_s.py
@@ -1,1 +1,15 @@
 import random
+user_wins = 0
+computer_wins = 0
+while True:
+    user_input = input("Type Rock / Paper / Scissor or 'q' to quit the game. ").lower()
+    # print(user_input)
+    # quit()
+    if user_input == "q":
+        break
+    if user_input not in ["rock","paper","scissor"]:
+        continue
+
+
+
+print("code is working fine.")
